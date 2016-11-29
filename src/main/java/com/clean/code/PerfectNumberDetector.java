8ee4888299529703fdadde7da09c4d14a6757288
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-11-29 22:13:13 +0530

    Clean Coding - GREEN (Added check for number equal to 0)

--- src/main/java/com/clean/code/PerfectNumberDetector.java
+++ src/main/java/com/clean/code/PerfectNumberDetector.java
@@ -2,8 +2,7 @@ package com.clean.code;
 
 public class PerfectNumberDetector {
 	
-	public String determinePerfectNumber(int number){
-		
-		return null;
+	public String determinePerfectNumber(int number){				
+		return (number == 0) ? "Input Number is not a Perfect Number." : null;
 	}
 }
