9758972c775808fe77e4fb857389e740235b2db5
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-11-29 22:32:21 +0530

    Clean Code - GREEN (Added implementation of Perfect Number detection logic)

--- src/main/java/com/clean/code/PerfectNumberDetector.java
+++ src/main/java/com/clean/code/PerfectNumberDetector.java
@@ -1,8 +1,39 @@
 package com.clean.code;
 
+/**
+ * Class for Perfect Number Detector.
+ * 
+ * @author hputhr
+ *
+ */
 public class PerfectNumberDetector {
 	
-	public String determinePerfectNumber(int number){				
-		return (number == 0) ? "Input Number is not a Perfect Number." : null;
+	/**
+	 * Method to determine if an input number is perfect or not.
+	 * 
+	 * @param int
+	 * @return String
+	 */
+	public String determinePerfectNumber(int numberToCheckForPerfectNumber){				
+		return (numberToCheckForPerfectNumber == 0) ? "Input Number is not a Perfect Number." : isNumberPerfect(numberToCheckForPerfectNumber);
+	}
+
+	/**
+	 * Method consisting of the logic to determine a Perfect Number.
+	 * 
+	 * @param int
+	 * @return String
+	 */
+	public String isNumberPerfect(int number) {
+		int sumOfDivisorsOfANumber = 0;
+		int numberToDivideInputNumber = 1;
+		
+		while(numberToDivideInputNumber < number){
+			if(number % numberToDivideInputNumber == 0){
+				sumOfDivisorsOfANumber += numberToDivideInputNumber;
+			}
+			numberToDivideInputNumber++;
+		}		
+		return (number == sumOfDivisorsOfANumber) ? "Input Number is a Perfect Number." : "Input Number is not a Perfect Number.";
 	}
 }
